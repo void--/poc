df8e9145211c6e60106493ec22607864d4c23b4a
Theo Ballew <tballew@talend.com>
2020-09-17 18:52:20 -0700

    don't send cli color codes to netlify

--- utils/talend/helpers/netlify-build.js
+++ utils/talend/helpers/netlify-build.js
@@ -2,19 +2,20 @@ const axios = require('axios');
 const chalk = require('chalk');
 
 module.exports = async function netlifyBuild(branch, env, preview) {
-    let message = preview
-        ? `Preview build of ${branch} branch with ${env} env triggered via CLI by ${process.env.DEVELOPER}`
-        : `Standard build of ${branch} branch with ${env} env triggered via CLI by ${process.env.DEVELOPER}`;
+    const message = (addColor) => {
+        const maybeYellow = (item) => addColor ? chalk.yellow(item) : item;
+        return `${preview ? "Preview" : "Standard"} build of ${maybeYellow(branch)} branch with ${maybeYellow(env)} env triggerd via CLI by ${maybeYellow(process.env.DEVELOPER)}`;
+    }
 
     try {
-        const buildUrl = `${process.env.NETLIFY_BUILD_HOOK}?trigger_branch=${branch}&trigger_title=${encodeURIComponent(message)}`;
+        const buildUrl = `${process.env.NETLIFY_BUILD_HOOK}?trigger_branch=${branch}&trigger_title=${encodeURIComponent(message())}`;
         const payload = JSON.stringify({
             'preview': preview,
             'env': env,
         });
         const response = await axios.post(buildUrl, payload);
         if (response.status === 200) {
-            console.log(chalk.green(message));
+            console.log(chalk.green(message(true)));
         }
     } catch (error) {
         console.error(chalk.red('Error:'), error);
