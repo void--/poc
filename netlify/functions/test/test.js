c578108f36a75920ba748fe8bb425f02d901a3b2
Theo Ballew <tballew@talend.com>
2020-09-27 15:48:28 -0700

    try directory format for function

--- netlify/functions/test/test.js
+++ netlify/functions/test/test.js
@@ -1,8 +1,6 @@
 exports.handler = async event => {
-    const test = event.queryStringParameters.test || 'asdf';
-    console.log('test!!!', process.env.SPACE_ID);
     return {
         statusCode: 200,
-        body: `~~~~~~> ${test}!`,
+        body: `~~~~~~> test!`,
     }
 }
