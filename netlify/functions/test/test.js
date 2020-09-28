95acb3ba71d31e54a7b93b2f4597f6cd50fe4d45
Theo Ballew <tballew@talend.com>
2020-09-27 23:30:48 -0700

    Update test function

--- netlify/functions/test/test.js
+++ netlify/functions/test/test.js
@@ -1,6 +1,14 @@
+const axios = require('axios');
+
 exports.handler = async event => {
+    const joke = await axios.get('https://icanhazdadjoke.com', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
     return {
         statusCode: 200,
-        body: `~~~~~~> test!`,
+        body: `old: ${joke.data.joke}`,
     }
 }
