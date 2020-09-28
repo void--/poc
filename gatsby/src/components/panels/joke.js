c0ee4e7347b6a7928686b6dccec305ee0852e364
Theo Ballew <tballew@talend.com>
2020-09-28 00:51:41 -0700

    fix joke component

--- gatsby/src/components/panels/joke.js
+++ gatsby/src/components/panels/joke.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { graphql } from "gatsby";
-import axios from
+import axios from "axios";
 
 export const fragment = graphql`
     fragment Joke on ContentfulJoke {
@@ -10,14 +10,29 @@ export const fragment = graphql`
 `;
 
 const Joke = (props) => {
-    const [joke, setJoke] = useState("...");
+    const [joke, setJoke] = useState("");
+    const [loading, setLoading] = useState(0);
 
     useEffect(() => {
+        if (!loading) return;
 
-    });
+        (async () => {
+            const joke = await axios.get("/api/test");
+            setJoke(joke.data);
+            setLoading(0);
+        })();
+
+    }, [loading]);
+
 
     return (
-        <div>joke</div>
+        <>
+            {joke
+                ? <><h2>This is a joke:</h2><div>{joke}</div></>
+                : <h2>Would you like a joke?</h2>
+            }
+            <button onClick={() => setLoading(1)}>{joke && "new"} joke please</button>
+        </>
     )
 }
 
